2884e22fe42f1b72b753a4e309f4c325f536fc85
Cheong Kar Wai <cheongkarwai5@gmail.com>
2024-01-07 22:31:14 +0800

    update configuration for deployment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
-    api_url:'https://car-booking-backend-bqwb.onrender.com',
+    api_url:'https://car-booking-backend-bqwb.onrender.com/api/v1',
     contact_number : '+60122969789',
     email_address:'gilbert.lee73@gmail.com',
     messenger_id:'gilbert.volvosisma',
